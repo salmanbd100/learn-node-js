164fd6f48419b904ded57748660c101fb0cb7d83
Salman Rahman <salman@Salmans-MacBook-Pro.local>
2019-09-15 00:35:28 +0600

    prettierrc fix
    Signed-off-by: Salman Rahman <salman@Salmans-MacBook-Pro.local>

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
-})
+  res.send('<h1>Hello World</h1>');
+});
 
 app.listen(PORT, () => {
-    console.log(`server is runnig ${PORT}`)
-})
+  // console.log(`server is runnig ${PORT}`)
+});
+
+let user =
+  '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+
+user = JSON.parse(user);
+
+console.log(user.isAdmin);
